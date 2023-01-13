f57457bf94384e9f3330a8fd356c1e4a5cd8ea33
Dylan Wilson <dwilson5817@gmail.com>
2023-01-13 20:04:43 +0000

    Initial commit

---
+++ bin/free-market-fandango-cdk.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib';
+import { FreeMarketFandangoCdkStack } from '../lib/free-market-fandango-cdk-stack';
+
+const app = new cdk.App();
+new FreeMarketFandangoCdkStack(app, 'FreeMarketFandangoCdkStack');
