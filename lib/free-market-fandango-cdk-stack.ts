014fbd21bab605e9962a7b28c8c27d96c643e326
Dylan Wilson <dwilson5817@gmail.com>
2023-01-15 22:33:53 +0000

    Add deployment for API
    Deploy Lambda and API Gateway to handle API requests.

--- lib/free-market-fandango-cdk-stack.ts
+++ lib/free-market-fandango-cdk-stack.ts
@@ -1,10 +1,12 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Stack, StackProps} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import * as path from "path";
 import { CloudFrontToS3 } from "@aws-solutions-constructs/aws-cloudfront-s3";
 import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 import { Constants } from "./constants";
+import { Function, Code, Runtime} from "aws-cdk-lib/aws-lambda";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class FreeMarketFandangoCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -22,6 +24,16 @@ export class FreeMarketFandangoCdkStack extends Stack {
     new BucketDeployment(this, 'FrontendBucketDeployment', {
       sources: [ Source.asset( path.join(__dirname, '../frontend') ) ],
       destinationBucket: cloudFrontToS3.s3BucketInterface,
+    });
+
+    const flaskHandler = new Function(this, 'FlaskHandler', {
+      runtime: Runtime.PYTHON_3_9,
+      handler: 'handler.lambda_handler',
+      code: Code.fromAsset( path.join(__dirname, 'lambda/lambda.zip') ),
+    });
+
+    new LambdaRestApi(this, 'FlaskApi', {
+      handler: flaskHandler
     });
   }
 }
