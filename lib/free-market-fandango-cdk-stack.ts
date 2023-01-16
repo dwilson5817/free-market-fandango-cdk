1cf9f583537ee7ec61f6f4e494164e1b72f498ae
Dylan Wilson <dwilson5817@gmail.com>
2023-01-16 00:15:53 +0000

    Revert "Add Lambda function to default VPC to enable access to RDS"
    This reverts commit 4dca5bbaf48fba9f9c66457ca7a87a51ca59d253.

--- lib/free-market-fandango-cdk-stack.ts
+++ lib/free-market-fandango-cdk-stack.ts
@@ -1,14 +1,12 @@
-import {Duration, Fn, Stack, StackProps} from 'aws-cdk-lib';
-import {Construct} from 'constructs';
-import {BucketDeployment, Source} from "aws-cdk-lib/aws-s3-deployment";
+import {Duration, Stack, StackProps} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import * as path from "path";
-import {CloudFrontToS3} from "@aws-solutions-constructs/aws-cloudfront-s3";
-import {Certificate} from "aws-cdk-lib/aws-certificatemanager";
-import {Constants} from "./constants";
-import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
-import {LambdaRestApi} from "aws-cdk-lib/aws-apigateway";
-import {Vpc} from "aws-cdk-lib/aws-ec2";
-import {Effect, ManagedPolicy, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import { CloudFrontToS3 } from "@aws-solutions-constructs/aws-cloudfront-s3";
+import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { Constants } from "./constants";
+import { Function, Code, Runtime} from "aws-cdk-lib/aws-lambda";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class FreeMarketFandangoCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -28,48 +26,11 @@ export class FreeMarketFandangoCdkStack extends Stack {
       destinationBucket: cloudFrontToS3.s3BucketInterface,
     });
 
-    const vpc = Vpc.fromVpcAttributes(this, 'DefaultVpc', {
-      vpcId: Constants.lambdaVpcId,
-      availabilityZones: Fn.getAzs(),
-      vpcCidrBlock: Constants.lambdaVpcSubnet
-    })
-
-    const lambdaRole = new Role(this, 'LambdaRole', {
-      assumedBy: new ServicePrincipal('lambda.amazonaws.com')
-    })
-
-    const managedPolicy = new ManagedPolicy(this, 'LambdaManagedPolicy', {
-      statements: [
-          new PolicyStatement({
-            effect: Effect.ALLOW,
-            actions: [
-              "ec2:CreateNetworkInterface",
-              "ec2:DescribeNetworkInterfaces",
-              "ec2:DeleteNetworkInterface",
-              "ec2:AssignPrivateIpAddresses",
-              "ec2:UnassignPrivateIpAddresses"
-            ],
-            resources: ['*']
-          }
-        )
-      ],
-      roles: [ lambdaRole ]
-    })
-
     const flaskHandler = new Function(this, 'FlaskHandler', {
       runtime: Runtime.PYTHON_3_9,
       handler: 'handler.lambda_handler',
       code: Code.fromAsset( path.join(__dirname, '../lambda.zip') ),
-      timeout: Duration.seconds(15),
-      environment: {
-        DATABASE_HOST: process.env.DATABASE_HOST || '',
-        DATABASE_PORT: process.env.DATABASE_PORT || '',
-        DATABASE_USER: process.env.DATABASE_USER || '',
-        DATABASE_PASS: process.env.DATABASE_PASS || '',
-        DATABASE_NAME: process.env.DATABASE_NAME || ''
-      },
-      vpc: vpc,
-      role: lambdaRole
+      timeout: Duration.seconds(15)
     });
 
     new LambdaRestApi(this, 'FlaskApi', {
