96a50f250fc1168904caebed682be96d325031ff
Dylan Wilson <dwilson5817@gmail.com>
2023-01-17 00:27:31 +0000

    Deploy admin password and secret key from environment
    These will be set in the environment of the Lambda function.

--- lib/free-market-fandango-cdk-stack.ts
+++ lib/free-market-fandango-cdk-stack.ts
@@ -42,11 +42,13 @@ export class FreeMarketFandangoCdkStack extends Stack {
       code: Code.fromAsset( path.join(__dirname, '../lambda.zip') ),
       timeout: Duration.seconds(15),
       environment: {
+        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '',
         DATABASE_HOST: process.env.DATABASE_HOST || '',
         DATABASE_PORT: process.env.DATABASE_PORT || '',
         DATABASE_USER: process.env.DATABASE_USER || '',
         DATABASE_PASS: process.env.DATABASE_PASS || '',
-        DATABASE_NAME: process.env.DATABASE_NAME || ''
+        DATABASE_NAME: process.env.DATABASE_NAME || '',
+        SECRET_KEY: process.env.SECRET_KEY || ''
       }
     });
 
