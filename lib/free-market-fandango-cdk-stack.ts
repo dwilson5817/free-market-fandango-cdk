0026a6fd85d44c828b2ab8c9e44a8a40f84286ba
Dylan Wilson <dwilson5817@gmail.com>
2023-01-20 20:17:44 +0000

    Use `aws-apigateway-lambda` construct instead
    Instead of manually creating the Lambda function and API Gateway, use
    the `aws-apigateway-lambda` construct which will include some default
    options recommended by AWS.

--- lib/free-market-fandango-cdk-stack.ts
+++ lib/free-market-fandango-cdk-stack.ts
@@ -1,12 +1,12 @@
-import {Duration, Stack, StackProps} from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import * as path from "path";
 import { CloudFrontToS3 } from "@aws-solutions-constructs/aws-cloudfront-s3";
 import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
 import { Constants } from "./constants";
-import { Function, Code, Runtime} from "aws-cdk-lib/aws-lambda";
-import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { Code, Runtime} from "aws-cdk-lib/aws-lambda";
+import { ApiGatewayToLambda } from "@aws-solutions-constructs/aws-apigateway-lambda";
 
 export class FreeMarketFandangoCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -35,28 +35,27 @@ export class FreeMarketFandangoCdkStack extends Stack {
       destinationBucket: cloudFrontToS3.s3BucketInterface,
     });
 
-    const flaskHandler = new Function(this, 'FlaskHandler', {
-      runtime: Runtime.PYTHON_3_9,
-      handler: 'handler.lambda_handler',
-      code: Code.fromAsset( path.join(__dirname, '../lambda.zip') ),
-      timeout: Duration.seconds(15),
-      environment: {
-        ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '',
-        DATABASE_HOST: process.env.DATABASE_HOST || '',
-        DATABASE_PORT: process.env.DATABASE_PORT || '',
-        DATABASE_USER: process.env.DATABASE_USER || '',
-        DATABASE_PASS: process.env.DATABASE_PASS || '',
-        DATABASE_NAME: process.env.DATABASE_NAME || '',
-        SECRET_KEY: process.env.SECRET_KEY || ''
-      }
-    });
-
-    new LambdaRestApi(this, 'FlaskApi', {
-      handler: flaskHandler,
-      domainName: {
-        domainName: Constants.apiDomainName,
-        certificate: certificate,
+    new ApiGatewayToLambda(this, 'ApiGatewayToLambdaPattern', {
+      lambdaFunctionProps: {
+        runtime: Runtime.PYTHON_3_9,
+        handler: 'handler.lambda_handler',
+        code: Code.fromAsset( path.join(__dirname, '../lambda.zip') ),
+        environment: {
+          ADMIN_PASSWORD: process.env.ADMIN_PASSWORD || '',
+          DATABASE_HOST: process.env.DATABASE_HOST || '',
+          DATABASE_PORT: process.env.DATABASE_PORT || '',
+          DATABASE_USER: process.env.DATABASE_USER || '',
+          DATABASE_PASS: process.env.DATABASE_PASS || '',
+          DATABASE_NAME: process.env.DATABASE_NAME || '',
+          SECRET_KEY: process.env.SECRET_KEY || ''
+        },
       },
+      apiGatewayProps: {
+        domainName: {
+          domainName: Constants.apiDomainName,
+          certificate: certificate,
+        }
+      }
     });
   }
 }
