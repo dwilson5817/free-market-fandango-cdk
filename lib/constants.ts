c6f513d9a1ae332bc5a386c334d0b5f6f6b40c69
Dylan Wilson <dwilson5817@gmail.com>
2023-10-05 20:32:28 +0000

    Update .gitlab-ci.yml to deploy to new AWS account
    A new AWS account will be used which is part of an organisation, this
    splits projects into seperate AWS accounts.  Further, OpenID Connect is
    now used instead of envrionment variable.  This allows short lived AWS
    credentials to be used instead of creating IAM users.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
     static readonly frontendDomainName: string = 'free-market-fandango.dylanwilson.dev'
     static readonly apiDomainName: string = 'free-market-fandango-api.dylanwilson.dev'
-    static readonly certificateArn: string = 'arn:aws:acm:us-east-1:351642118781:certificate/61c6ced2-d2f5-424f-921b-eb878987262b'
+    static readonly certificateArn: string = 'arn:aws:acm:eu-west-1:805294971713:certificate/9039e353-2f25-4fdf-9404-b4b13412a111'
 }
