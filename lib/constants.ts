dd1c256f1c9da88b8c66f73fb477191112e96bae
Dylan Wilson <dwilson5817@gmail.com>
2023-10-05 21:04:36 +0000

    Move certificate from eu-west-1 to us-east-1
    CDK requires the certificate be in this location.

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export class Constants {
     static readonly frontendDomainName: string = 'free-market-fandango.dylanwilson.dev'
     static readonly apiDomainName: string = 'free-market-fandango-api.dylanwilson.dev'
-    static readonly certificateArn: string = 'arn:aws:acm:eu-west-1:805294971713:certificate/9039e353-2f25-4fdf-9404-b4b13412a111'
+    static readonly certificateArn: string = 'arn:aws:acm:us-east-1:805294971713:certificate/381e458d-9c13-442f-93ff-79f727a8b12c'
 }
