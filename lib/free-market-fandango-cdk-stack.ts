606b252b7fb5ed94e89408c6ef42a7072e41f7d4
Dylan Wilson <dwilson5817@gmail.com>
2026-04-10 16:45:24 +0000

    feat(cdk): add S3 bucket for storing Lambda artifacts
    Will migrate this project to use the standard CI/CD components which are
    built to publish the artifact to S3, then to be picked up in the CDK.
    
    Firstly, though, we need to create the artifacts bucket.

--- lib/free-market-fandango-cdk-stack.ts
+++ lib/free-market-fandango-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as s3 from "aws-cdk-lib/aws-s3";
 import { CronConstruct } from "./constructs/cron-construct";
 import { ApiConstruct } from "./constructs/api-construct";
 import { EventQueueConstruct } from "./constructs/event-queue-construct";
@@ -8,6 +9,16 @@ import { EventQueueConstruct } from "./constructs/event-queue-construct";
 export class FreeMarketFandangoCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const artifactsBucket = new s3.Bucket(this, 'ArtifactsBucket', {
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      lifecycleRules: [{
+        noncurrentVersionExpiration: cdk.Duration.days(1),
+        noncurrentVersionsToRetain: 5,
+      }],
+    });
 
     const dataTable = new dynamodb.TableV2(this, 'Data', {
       partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
